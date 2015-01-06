568589c1bcb0341f93608e1d78a564b1871e1326
Daniel Yee <yeedan@gmail.com>
2015-01-05 18:02:41 -0800

    game sanitize method

--- app/controllers/game_controller.rb
+++ app/controllers/game_controller.rb
@@ -15,18 +15,27 @@ class GameController < ApplicationController
     state.tech_points = 0
     state.hit_points = 10
     state.enemy_deck = 1
-    state.hand = []
-    state.my_deck = [1, 1, 1, 2, 2, 2, 2, 2, 2, 2]
-    state.discard = []
+    state.hand = ""
+    state.my_deck = "1 1 1 2 2 2 2 2 2 2"
+    state.discard = ""
     state.turn_phase = 0
 
-    state.my_deck.shuffle!
+    state.my_deck = str_to_arr(state.my_deck)
     deal_starting_hand(state)
   end
 
+  # takes "1 2 3 4" turns into [1,2,3,4]
+  def str_to_arr(string)
+    arr_of_strings = string.split(" ")
+    arr_of_strings.map! do |element|
+      element.to_i
+    end
+  end
+
   def deal_starting_hand(state)
+    deck_arr = str_to_arr(state.my_deck)
     5.times do |card|
-      state.hand << state.my_deck.shift
+      state.hand << deck_arr.shift
     end
   end
 
