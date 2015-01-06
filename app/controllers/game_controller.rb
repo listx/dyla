782aeb42b56138c1c893cda71501e34111a1a449
Daniel Yee <yeedan@gmail.com>
2015-01-05 17:30:06 -0800

    initial game setup methods

--- app/controllers/game_controller.rb
+++ app/controllers/game_controller.rb
@@ -1,2 +1,24 @@
 class GameController < ApplicationController
+  def create
+    @new_state = Gamestate.new
+    @new_state = sanitize(@new_state)
+  end
+
+
+  private
+
+  def sanitize(state)
+    state.victory_points = 0
+    state.tech_points = 0
+    state.hit_points = 10
+    state.enemy_deck = 1
+    state.hand = []
+    state.my_deck = [1, 1, 1, 2, 2, 2, 2, 2, 2, 2]
+    state.discard = []
+    state.turn_phase = 0
+  end
+
+  # def game_state_params
+  #   params.require(:gameState).permit()
+  # end
 end
