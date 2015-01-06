ed4893534791c5897473128c32cc231a9e7eec8c
Daniel Yee <yeedan@gmail.com>
2015-01-05 16:26:44 -0800

    gamestate migration

--- db/migrate/20150105235824_create_gamestates.rb
+++ db/migrate/20150105235824_create_gamestates.rb
@@ -1,7 +1,16 @@
 class CreateGamestates < ActiveRecord::Migration
   def change
     create_table :gamestates do |t|
-
+      t.integer :victory_points
+      t.integer :tech_points
+      t.integer :hit_points
+      t.integer :enemy_deck
+      # a string to hold all cards in your hand, deck, and discard
+      # "1 12 3 4 15 6 20 8" --> [1,12,3,4,15,6,20,8] for Ruby
+      t.text :hand
+      t.text :my_deck
+      t.text :discard
+      t.integer :turn_phase
       t.timestamps null: false
     end
   end
