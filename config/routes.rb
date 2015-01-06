59e1c7c164f0c6c38724d877c56e59b23c473e00
Daniel Yee <yeedan@gmail.com>
2015-01-05 17:42:54 -0800

    adding rules path

--- config/routes.rb
+++ config/routes.rb
@@ -8,6 +8,7 @@ Rails.application.routes.draw do
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
   get 'creategame' => 'game#create'
+  get 'gamerules' => 'game#rules'
 
 
   # Example of named route that can be invoked with purchase_url(id: product.id)
