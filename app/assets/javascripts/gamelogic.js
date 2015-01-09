0b7a671ea2db655dee063a3ddb08eb407e24cc44
Daniel Yee <yeedan@gmail.com>
2015-01-08 23:31:19 -0800

    starting to write ajax call for cards jsons

--- app/assets/javascripts/gamelogic.js
+++ app/assets/javascripts/gamelogic.js
@@ -1,5 +1,19 @@
 $(document).ready(function() {
   $('#enemy-cards').on('click', testFunc);
+  $('#enemypicture').ready(setupCards);
+
+  function setupCards(e) {
+    var enemycards = $.ajax({
+      url: '/gamestates/1'
+    })
+    .done(function(response) {
+      console.log('success');
+      //need to get expected jsons and do something with them
+    })
+    .fail(function(event) {
+      console.log('You FAILED!!!');
+    })
+  }
 
   function testFunc() {
     console.log('successful test');
