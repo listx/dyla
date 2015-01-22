0c19bc59795ec6be1f37d4d485ad6a175239c993
Daniel Yee <yeedan@gmail.com>
2015-01-21 21:53:31 -0800

    scripts for setup, shuffle, draw

--- app/assets/javascripts/gamelogic.js
+++ app/assets/javascripts/gamelogic.js
@@ -1,5 +1,6 @@
 $(document).ready(function() {
-  $('#enemypicture').on('click', testFunc);
+  $('#enemypicture').on('click', playerSetup);
+  $('#enemypicture').ready(testFunc);
 
   function testFunc() {
     console.log('successful test');
@@ -13,4 +14,39 @@ $(document).ready(function() {
 
   var attackcardslist = $.parseJSON($('#ac').attr('datastuff'))
   var tacticscardslist = $.parseJSON($('#tc').attr('datastuff'))
+
+  var playerDeck = [];
+  var playerHand = [];
+
+  function playerSetup(){
+    var start = ["tc1","tc1","tc1","ac1","ac1","ac1","ac1","ac1","ac1","ac1"];
+    shuffle(start);
+
+    for (i = 0; i < 5; i++) {
+      playerHand.push(start.shift());
+    }
+    playerDeck = start;
+  }
+
+  function shuffle(array) {
+    var currentIndex = array.length, temporaryValue, randomIndex ;
+
+    // While there remain elements to shuffle...
+    while (0 !== currentIndex) {
+
+      // Pick a remaining element...
+      randomIndex = Math.floor(Math.random() * currentIndex);
+      currentIndex -= 1;
+
+      // And swap it with the current element.
+      temporaryValue = array[currentIndex];
+      array[currentIndex] = array[randomIndex];
+      array[randomIndex] = temporaryValue;
+      }
+      return array;
+  }
+
+  function drawCard(){
+    playerHand.push(playerDeck.shift());
+  }
 });
