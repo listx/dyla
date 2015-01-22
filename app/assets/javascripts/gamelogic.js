cdf960da2c4bb72fb5f93b19999f35f4df6d19e9
Daniel Yee <yeedan@gmail.com>
2015-01-21 22:40:09 -0800

    cleanup, working on enemy panel

--- app/assets/javascripts/gamelogic.js
+++ app/assets/javascripts/gamelogic.js
@@ -1,11 +1,6 @@
 $(document).ready(function() {
   $('#enemypicture').on('click', playerSetup);
-  $('#enemypicture').ready(testFunc);
-
-  function testFunc() {
-    console.log('successful test');
-    $('#enemycardsreference').append("hi"+enemycards1list[1].name+" there");
-  }
+  $('#enemypicture').on('click', enemySetup);
 
   var enemycards1list = $.parseJSON($('#ec1').attr('datastuff'))
   console.log(enemycards1list);
@@ -17,9 +12,77 @@ $(document).ready(function() {
 
   var playerDeck = [];
   var playerHand = [];
+  var enemyDeck1 = [];
+  var enemyDeck2 = [];
+  var enemyDeck3 = [];
+  var currentEnemy = "";
+
+  function enemySetup(){
+    //deck1
+    for (i = 0; i < 7; i++) {
+      enemyDeck1.push(enemycards1list[0]);
+    }
+    for (i = 0; i < 5; i++) {
+      enemyDeck1.push(enemycards1list[1]);
+    }
+    for (i = 0; i < 5; i++) {
+      enemyDeck1.push(enemycards1list[2]);
+    }
+    for (i = 0; i < 2; i++) {
+      enemyDeck1.push(enemycards1list[3]);
+    }
+    //deck2
+    for (i = 0; i < 7; i++) {
+      enemyDeck2.push(enemycards2list[0]);
+    }
+    for (i = 0; i < 5; i++) {
+      enemyDeck2.push(enemycards2list[1]);
+    }
+    for (i = 0; i < 5; i++) {
+      enemyDeck2.push(enemycards2list[2]);
+    }
+    for (i = 0; i < 2; i++) {
+      enemyDeck2.push(enemycards2list[3]);
+    }
+    //deck 3
+    for (i = 0; i < 7; i++) {
+      enemyDeck3.push(enemycards3list[0]);
+    }
+    for (i = 0; i < 5; i++) {
+      enemyDeck3.push(enemycards3list[1]);
+    }
+    for (i = 0; i < 5; i++) {
+      enemyDeck3.push(enemycards3list[2]);
+    }
+    for (i = 0; i < 1; i++) {
+      enemyDeck3.push(enemycards3list[3]);
+    }
+    for (i = 0; i < 1; i++) {
+      enemyDeck3.push(enemycards3list[4]);
+    }
+    for (i = 0; i < 1; i++) {
+      enemyDeck3.push(enemycards3list[5]);
+    }
+    shuffle(enemyDeck1);
+    shuffle(enemyDeck2);
+    shuffle(enemyDeck3);
+    //starting enemy
+    drawEnemy(enemyDeck1);
+    console.log(currentEnemy);
+    showEnemy(currentEnemy);
+  }
+
+  function drawEnemy(deck){
+    currentEnemy = deck.shift();
+  }
+
+  function showEnemy(card){
+    $('#enemypicture').append("<p>"+card.name+"</p>");
+    $('#enemystats').append("<div class='enemycard'>name:"+card.name+"<br>hp:"+card.hp+"<br>damage:"+card.damage+"<br>tech points:"+card.tp+"<br>victory points:"+card.vp+"<br>text:"+card.description+"</div>");
+  }
 
   function playerSetup(){
-    var start = ["tc1","tc1","tc1","ac1","ac1","ac1","ac1","ac1","ac1","ac1"];
+    var start = [tacticscardslist[0],tacticscardslist[0],tacticscardslist[0],attackcardslist[0],attackcardslist[0],attackcardslist[0],attackcardslist[0],attackcardslist[0],attackcardslist[0],attackcardslist[0]];
     shuffle(start);
 
     for (i = 0; i < 5; i++) {
